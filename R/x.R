948fa0e4459cc6cc246f9cb94e27d1c1500914ba
larsmew <larsmew@gmail.com>
2016-07-19 15:04:11 -0700

    Added all files related to R package to repo

---
+++ R/x.R
@@ -0,0 +1,13 @@
+#' A bunch of radii (TITLE)
+#'
+#' Really, do you need this explained?
+#' Well, ok. (DESCRIPTION)
+#'
+#' @format description of the variables,
+#'   with full names and explanations (esp
+#'   for data frames). Dimensions help.
+#'
+#' @source where did the data come from?
+#'   This could be a URL.
+#' @name x
+NULL
